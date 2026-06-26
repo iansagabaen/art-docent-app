a7b71670793431db38798353b8a2ba016ff873d0
Ian Sagabaen <iansagabaen@gmail.com>
2026-06-25 20:43:42 -0700

    Use Jan 1, 2026 test date - shows upcoming classes for testing

--- generate-next-class.js
+++ generate-next-class.js
@@ -90,7 +90,8 @@ async function generateNextClassJson() {
     })
 
     // Find upcoming classes (future dates)
-    const today = new Date()
+    // For testing: use Jan 1, 2026 to see upcoming classes
+    const today = new Date('2026-01-01')
     today.setHours(0, 0, 0, 0)
 
     const upcoming = ianRows.filter(row => {
